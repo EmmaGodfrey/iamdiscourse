f71647910ebd164c0f282e5ae24a0a59012dde22
EmmaGodfrey <emmanuekgodfrey4@gmail.com>
2024-08-05 13:56:33 +0200

    Update settings.py

--- iamdiscourse/settings.py
+++ iamdiscourse/settings.py
@@ -95,9 +95,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Security settings
-SECURE_SSL_REDIRECT = not DEBUG  # Redirect to HTTPS in production
-CSRF_COOKIE_SECURE = not DEBUG  # CSRF cookie secure in production
-SESSION_COOKIE_SECURE = not DEBUG  # Session cookie secure in production
+SECURE_SSL_REDIRECT = True  # Redirect to HTTPS in production
+CSRF_COOKIE_SECURE = True  # CSRF cookie secure in production
+SESSION_COOKIE_SECURE = True  # Session cookie secure in production
 X_FRAME_OPTIONS = 'DENY'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
