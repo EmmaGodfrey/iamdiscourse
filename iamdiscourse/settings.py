0d59402b8427cd2cc077734b6cd56910e54287fd
EmmaGodfrey <emmanuekgodfrey4@gmail.com>
2024-08-09 18:33:22 +0200

    updates

--- iamdiscourse/settings.py
+++ iamdiscourse/settings.py
@@ -26,7 +26,10 @@ DATABASES = {
 
 # Application definition
 INSTALLED_APPS = [
-    'jazzmin',  # Add django-jazzmin here
+     # General use templates & template tags (should appear first)
+    'adminlte3',
+     # Optional: Django admin theme (must be before django.contrib.admin)
+    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
